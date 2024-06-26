54f0ca1c2d9122306dea4e392da3329a8e229d15
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-25 22:19:05 -0400

    all CRUD routes made

--- routes/checkForms.js
+++ routes/checkForms.js
@@ -3,7 +3,16 @@ const formsCtrl = require('../controllers/forms.js');
 
 const router = Router();
 
-//user POST route
+//POST form route
 router.post('/check-in', formsCtrl.createForm);
+
+//GET form  route
+router.get('/check-in', formsCtrl.getForms);
+
+//PATCH form route by ID
+router.patch('/check-in/:id', formsCtrl.updateForm);
+
+//DELETE form route by ID
+router.delete('/check-in/:id', formsCtrl.updateForm);
 
 module.exports = router;
