6345d6eb90d8da9bb68b55b6599237f2fed4603c
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-25 22:25:25 -0400

    get request function

--- controllers/forms.js
+++ controllers/forms.js
@@ -2,9 +2,10 @@ const Form = require('../models/Form');
 
 module.exports = {
   createForm,
+  getForms,
 };
 
-//POST request function for user
+//POST request function for form
 async function createForm(req, res) {
   try {
     const form = await Form.create(req.body);
@@ -12,5 +13,16 @@ async function createForm(req, res) {
     res.status(200).json(form);
   } catch (err) {
     res.status(400).json('No Beuno:(');
+  }
+}
+
+//GET request function for form
+async function getForms(req, res) {
+  try {
+    const forms = await Form.find({});
+
+    res.status(200).json(forms);
+  } catch (err) {
+    res.status(400).send(err);
   }
 }
